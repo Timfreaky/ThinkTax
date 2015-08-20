4c8261a17e49f15b80207ee7a84b38e8cabe177d
Timfreaky <timmcmonney@hotmail.com>
2015-08-20 20:23:50 +0200

    WordPress Folder
    Everything you need to continue the site in its actual state

---
+++ InstantWP_4.4.2/iwpserver/htdocs/wordpress/wp-content/themes/enigma/nocontent.php
@@ -0,0 +1,8 @@
+<div class="col-md-8">
+	<div class="error_404">
+		<h2><?php _e('404','weblizar'); ?></h2>
+		<h4><?php _e('Whoops... Page introuvable !!!','weblizar'); ?></h4>
+		<p><?php _e('Nous sommes désolés, mais la page que vous recherchez n'existe pas.','weblizar'); ?></p>
+		<p><a href="<?php echo home_url( '/' ); ?>"><button class="enigma_send_button" type="submit"><?php _e('Go To Homepage','weblizar'); ?></button></a></p>
+	</div>
+</div>
